4b33de53d32d020a7f868dc280747e8107fb26af
ngckinhluan <nkluantran2907@gmail.com>
2024-06-08 11:14:42 +0700

    first commit

---
+++ ManageCategoriesApp/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManageCategoriesApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    ManageCategories _categories = new ManageCategories();
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        LoadCategories();
+        if (lvCategories.Items.Count > 0)
+        {
+            lvCategories.SelectedIndex = 0;
+        }
+    }
+    private void LoadCategories() => lvCategories.ItemsSource = _categories.GetCategories();
+    private void btnInsert_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            Category category = new Category { CategoryName = txtCategoryName.Text };
+            _categories.InsertCategory(category);
+            LoadCategories();
+        } catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Insert Category");
+        }
+     
+    }
+
+    private void btnUpdate_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (int.TryParse(txtCategoryID.Text, out int categoryId))
+            {
+                var category = new Category
+                {
+                    CategoryID = categoryId,
+                    CategoryName = txtCategoryName.Text
+                };
+                _categories.UpdateCategory(category);
+                LoadCategories();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Category ID", "Update Category");
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Update Category");
+        }
+    }
+
+    private void btnDelete_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (int.TryParse(txtCategoryID.Text, out int categoryId))
+            {
+                var category = new Category
+                {
+                    CategoryID = categoryId,
+                    CategoryName = txtCategoryName.Text
+                };
+                _categories.DeleteCategory(category);
+                LoadCategories();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Category ID", "Delete Category");
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Delete Category");
+        }
+    }
+    
+    private void btnLoad_Click(object sender, RoutedEventArgs e)
+    {
+        LoadCategories();
+    }
+
+}
---
+++ ManageCategoriesApp/ManageCategories.cs
@@ -0,0 +1,115 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ManageCategoriesApp;
+
+public record Category
+{
+    public int CategoryID { get; set; }
+    public string CategoryName { get; set; }
+}
+
+public class ManageCategories
+{
+    private SqlConnection _connection;
+    private SqlCommand _command;
+    private string ConnectionString = "Server=(local);uid=sa;pwd=12345;database=MyStore;TrustServerCertificate=True";
+
+    public List<Category> GetCategories()
+    {
+        List<Category> categories = new List<Category>();
+        _connection = new SqlConnection(ConnectionString);
+        string query = "SELECT * FROM Categories";
+        _command = new SqlCommand(query, _connection);
+        try
+        {
+            _connection.Open();
+            SqlDataReader reader = _command.ExecuteReader(CommandBehavior.CloseConnection);
+            if (reader.HasRows == true)
+            {
+                while (reader.Read())
+                {
+                    categories.Add(new Category
+                    {
+                        CategoryID = (int)reader["CategoryID"],
+                        CategoryName = reader.GetString("CategoryName")
+                    });
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            _connection.Close();
+        }
+
+        return categories;
+    }
+
+    public void InsertCategory(Category category)
+    {
+        _connection = new SqlConnection(ConnectionString);
+        _command = new SqlCommand("INSERT INTO Categories VALUES(@CategoryName)", _connection);
+        _command.Parameters.AddWithValue("@CategoryName", category.CategoryName); // Add this line
+        try
+        {
+            _connection.Open();
+            _command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            _connection.Close();
+        }
+    }
+
+    public void UpdateCategory(Category category)
+    {
+        _connection = new SqlConnection(ConnectionString);
+        string query = "UPDATE Categories SET CategoryName = @CategoryName WHERE CategoryID = @CategoryID";
+        _command = new SqlCommand(query, _connection);
+        _command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+        _command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+        try
+        {
+            _connection.Open();
+            _command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            _connection.Close();
+        }
+    }
+
+    public void DeleteCategory(Category category)
+    {
+        _connection = new SqlConnection(ConnectionString);
+        string query = "DELETE Categories WHERE CategoryID = @CategoryID";
+        _command = new SqlCommand(query, _connection);
+        _command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+        try
+        {
+            _connection.Open();
+            _command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            _connection.Close();
+        }
+    }
+   
+}
